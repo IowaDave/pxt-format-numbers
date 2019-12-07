df3176a558e790a4d1d795e381fc873defd32694
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-12-07 11:56:49 -0800

    Initial code

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # pxt-format-numbers
+Custom blocks to format numbers as strings.
 
+### Blocks included
+**formatDecimal(x, n)** returns a string containing 
+the number, <x>, rounded to <n> decimal places.
 
 
 ## TODO
 
-- [ ] Add a reference for your blocks here
-- [ ] Add "icon.png" image (300x200) in the root folder
-- [ ] Add "- beta" to the GitHub project description if you are still iterating it.
-- [ ] Turn on your automated build on https://travis-ci.org
+- [x] Add a reference for your blocks here
 - [ ] Use "pxt bump" to create a tagged release on GitHub
 - [ ] On GitHub, create a new file named LICENSE. Select the MIT License template.
 - [ ] Get your package reviewed and approved https://makecode.microbit.org/extensions/approval
--- main.ts
+++ main.ts
@@ -1,7 +1,22 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace format {
+    /**
+     * Return a formatted string containing
+     * a number rounded to n decimal places,
+     * padded with zeros after the decimal point
+     * if that is how the rounding comes out.
+     * @param x is the number to be formatted, eg: 0
+     * @param n is the number of decimal places, eg: 2 
+     */
+    //% block='format|%x|to string having|%n|decimal places'
+    export function formatDecimal(x: number, n: number): string {
+        let workingValue = x * Math.pow(10, n);
+        workingValue = Math.round(workingValue);
+        let workingString = '' + `${workingValue}`
+        return workingString.slice(0, -n)
+            + '.' + workingString.slice(-n);
+    }
+}
