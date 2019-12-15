44e59a2f148a40babcc3a4678aaf5ca2886655f7
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-12-15 13:54:27 -0800

    Remove convert-to-hex

--- main.ts
+++ main.ts
@@ -27,39 +27,4 @@ namespace format {
             + '.' + workingString.slice(-n);
     }
 
-    /**
-     * Return a number in the range 0 to + 2**32 - 1
-     * as a string of hex digits
-     * @param x is the number to be formatted, eg: 0
-     */
-    //% block='format|%x| to hex string using|%case'
-    export function formatHex(x: number, usingCase: HexCase): string {
-        let workingValue = x;
-        let hexDigits = 2; // default to 8-bit
-        let workingString = '';
-        let charPointer = 0;
-        let hexChars: string[] = ['0', '1', '2', '3',
-            '4', '5', '6', '7',
-            '8', '9', 'a', 'b',
-            'c', 'd', 'e', 'f',
-            'A', 'B', 'C', 'D', 'E', 'F'];
-        // test for allowable value
-        if ((x < 0) && (x <= ((2 ** 31) - 1))) {
-            return 'TooLarge';
-        }
-        // determine number of hex digits
-        if (x > 2 ** 8) { hexDigits += 2; } // 16 bits
-        if (x > 2 ** 16) { hexDigits += 2; } // 24 bits
-        if (x > 2 ** 24) { hexDigits += 2; } // 32 bits
-        // convert to hex
-        for (let counter = 0; counter < hexDigits; counter++) {
-            charPointer = workingValue & 15;
-            if (usingCase == HexCase.ABCDEF) { charPointer += 6; }
-            workingString = hexChars[charPointer] + workingString;
-            workingValue = workingValue >> 4;
-        }
-
-        return workingString;
-
-    }
 }
--- test.ts
+++ test.ts
@@ -1,2 +1,1 @@
 basic.showString(format.formatDecimal(123.045, 1));
-basic.showString(format.formatHex(2147483646, HexCase.abcdef));
